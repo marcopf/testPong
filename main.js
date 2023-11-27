89f5e0946dfcf39d2c07072beae336bdbbba20eb
marco <marco@pop-os.localdomain>
2023-11-27 22:23:59 +0100

    now is decent

--- main.js
+++ main.js
@@ -1,93 +1,135 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+function mapValue(x, in_min, in_max, out_min, out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+}
+
+const GAME_WIDTH =220;
+const GAME_HEIGHT = 250;
+const PILLS_Y = 6;
+const PILLS_OFFSET = (GAME_HEIGHT - 20) / 2;
+const BALL_Y = 6;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, 300 / 300, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(0, 1, 0);
-scene.add(directionalLight);
-renderer.setSize( 300, 300);
-document.body.appendChild( renderer.domElement );
-const texture = new THREE.TextureLoader().load( './crate.gif' );
-texture.colorSpace = THREE.SRGBColorSpace;
-
-const geometryS = new THREE.SphereGeometry(0.6, 10, 10);
-const materialS = new THREE.MeshBasicMaterial( { map: texture } );
-const sphere = new THREE.Mesh( geometryS, materialS );
-
-const geometry = new THREE.CylinderGeometry(0.3, 0.3, 2, 4 );
-const material = new THREE.MeshBasicMaterial( { map: texture } );
-const cube = new THREE.Mesh( geometry, material );
-
-const radius = 1;
-const height = 3;
-const segments = 32;
-
-const topGeometry = new THREE.SphereGeometry(radius, segments, segments, 0, Math.PI * 2, 0, Math.PI / 2);
-const bottomGeometry = new THREE.SphereGeometry(radius, segments, segments, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2);
-const middleGeometry = new THREE.CylinderGeometry(radius, radius, height - 2 * radius, segments);
-
-// Merge geometries using BufferGeometryUtils
-const pillGeometry = BufferGeometryUtils.mergeBufferGeometries([topGeometry, bottomGeometry, middleGeometry]);
-
-// Create a material
-const materialPill = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-// Create a mesh with the pill geometry and material
-const pillMesh = new THREE.Mesh(pillGeometry, materialPill);
-
-cube.rotation.z = Math.PI / 2
-cube.position.set(0, -5, 0)
-
-cubeT.rotation.z = Math.PI / 2
-cubeT.position.set(0, 5, 0)
-
-scene.add( cube );
-scene.add(pillMesh);
-scene.add(sphere);
-camera.position.z = 10;
-
+//defining background
+const background = new THREE.TextureLoader().load('./textures/tenniBack.png');
+scene.background = background
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//controls.update() must be called after any manual changes to the camera's transform
-// camera.position.set( 0, 20, 100 );
-let delta = 0.05;
-let deltaX = 0.05;
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 0, 90, 170 );
+controls.update();
+
+//define ball
+const textureBall = new THREE.TextureLoader().load('./textures/ball.jpg');
+textureBall.colorSpace = THREE.SRGBColorSpace;
+const geometryBall = new THREE.SphereGeometry(5, 10, 20, 20);
+const materialBall =new THREE.MeshStandardMaterial( {map: textureBall, side: THREE.DoubleSide, roughness: 0.7, metalness: 0.1,} );
+const ball = new THREE.Mesh( geometryBall, materialBall);
+ball.position.set(0, 6, 0);
+scene.add(ball);
+
+//define plane
+const texturePlane = new THREE.TextureLoader().load('./textures/blue-tennis-court-texture-synthetic-surface-tennis-court-sports-background-field-top-view-ground_611669-82.avif');
+texturePlane.colorSpace = THREE.SRGBColorSpace;
+const geometryPlane = new THREE.PlaneGeometry(GAME_WIDTH, GAME_HEIGHT);
+const materialPlane =new THREE.MeshStandardMaterial( {map: texturePlane, side: THREE.DoubleSide, roughness: 0.7, metalness: 0.1,} );
+const plane = new THREE.Mesh( geometryPlane, materialPlane);
+plane.rotation.x = Math.PI / 2;
+scene.add(plane);
+
+//define bottom bar
+const texture = new THREE.TextureLoader().load('./textures/pddleTexture.jpg');
+texture.colorSpace = THREE.SRGBColorSpace;
+const geometryBottomPills = new THREE.CapsuleGeometry( 4, 20, 10, 10 ); 
+const materialBottomPills = new THREE.MeshStandardMaterial({ map: texture, roughness: 0.7, metalness: 0.1,});
+const bottomPills = new THREE.Mesh( geometryBottomPills, materialBottomPills);
+bottomPills.rotation.z = Math.PI / 2;
+bottomPills.position.set(0, PILLS_Y, PILLS_OFFSET);
+scene.add( bottomPills );
+
+//define upper bar
+const geometryTopPills = new THREE.CapsuleGeometry( 4, 20, 10, 10 ); 
+const materialTopPills = new THREE.MeshStandardMaterial({ map: texture, roughness: 0.7, metalness: 0.1,});
+const topPills = new THREE.Mesh( geometryTopPills, materialTopPills);
+topPills.rotation.z = Math.PI / 2;
+topPills.position.set(0, PILLS_Y, -PILLS_OFFSET);
+scene.add( topPills );
+
+
+//setting up lights
+const light1 = new THREE.PointLight(0xFFFFFF, 100000);
+light1.position.set(100, 100, 100);
+scene.add(light1);
+const light2 = new THREE.PointLight(0xFFFFFF, 100000);
+light2.position.set(-100, 100, -100);
+scene.add(light2);
+
+//define ball movement
+let deltaZ = 0.0;
+let deltaX = 0.0;
+ball.rotation.z = 0.01;
+ball.rotation.x = 0.01;
+
+//defining wall
+
+
+console.log("done setting up")
 function animate() {
 	requestAnimationFrame( animate );
-	const box1 = new THREE.Box3().setFromObject(cube);
-	const box2 = new THREE.Box3().setFromObject(cubeT);
-	const sphereB = new THREE.Box3().setFromObject(sphere);
-	if ((sphereB.intersectsBox(box2) || sphereB.intersectsBox(box1)) || (sphere.position.y > 5 || sphere.position.y < -5)) {
-		delta = -delta
+	if (deltaX != 0 && deltaZ != 0)
+	{
+		deltaX > 0 ? deltaX += 0.0001 : deltaX -= 0.0001;
+		deltaZ > 0 ? deltaZ += 0.0001 : deltaZ -= 0.0001;
 	}
-	if ((sphere.position.x > 5 || sphere.position.x < -5))
-		deltaX = -deltaX
-	sphere.position.set(sphere.position.x + deltaX, sphere.position.y + delta, 0)
+	const ballColl = new THREE.Box3().setFromObject(ball);
+	const topPillsColl = new THREE.Box3().setFromObject(topPills);
+	const botPillsColl = new THREE.Box3().setFromObject(bottomPills);
+	if (ball.position.z > PILLS_OFFSET || ball.position.z < -PILLS_OFFSET)
+	{
+		deltaZ = 0;
+		deltaX = 0;
+		ball.position.set(0, 0, 0)
+		alert("You loose !!");
+	}
+	if ((ballColl.intersectsBox(topPillsColl) || ballColl.intersectsBox(botPillsColl))) {
+		deltaZ = -deltaZ;
+	}
+	if (ball.position.x > ((GAME_WIDTH - 20) / 2) || ball.position.x < ((GAME_WIDTH - 20) / -2))
+		deltaX = -deltaX;
+	controls.update();
 	renderer.render( scene, camera );
-	console.log(sphere.position.x)
+	topPills.position.set(ball.position.x, PILLS_Y, topPills.position.z)
+	ball.rotation.z += 0.05;
+	ball.rotation.x = 0.05;
+	ball.position.set(ball.position.x += deltaX, BALL_Y, ball.position.z += deltaZ);
+
 }
 
 window.addEventListener("keyup", (key)=>{
-	if (key.key == "ArrowRight")
-		cube.position.set( cube.position.x + 0.7, -5, 0 );
-	if (key.key == "ArrowLeft")
-		cube.position.set( cube.position.x - 0.7, -5, 0 );
-	if (key.key == "a")
-		cubeT.position.set( cubeT.position.x - 0.7, 5, 0 );
-	if (key.key == "d")
-		cubeT.position.set( cubeT.position.x + 0.7, 5, 0 );
-		if (key.key == "w")
-		cubeT.position.set( 0, cubeT.position.y + 0.7, 0 );
-		if (key.key == "s")
-		cubeT.position.set( 0, cubeT.position.y - 0.7, 0 );
+	if (key.key == "s")
+	{
+		deltaZ = 0.2;
+		deltaX = 0.07;
+	}
+	if (key.key == "ArrowLeft" && bottomPills.position.x >= ((GAME_WIDTH - 40) / -2))
+		bottomPills.position.set( bottomPills.position.x - 10 , PILLS_Y, bottomPills.position.z);
+	if (key.key == "ArrowRight" && bottomPills.position.x <= ((GAME_WIDTH - 40) / 2))
+		bottomPills.position.set( bottomPills.position.x + 10 , PILLS_Y, bottomPills.position.z);
+})
+
+window.addEventListener("mousemove", (e)=>{
+	bottomPills.position.set(mapValue(e.x, 0, window.innerWidth, -PILLS_OFFSET, PILLS_OFFSET), PILLS_Y, bottomPills.position.z)
 })
 
 animate();
